c8413c51f951b8b38c9a9bd1242a299e8feaec3b
Hengkiardo <hengki.sihombing@gmail.com>
2014-05-23 17:43:41 +0700

    add config example

---
+++ app/config/config.example.js
@@ -0,0 +1,85 @@
+// config for the express app
+// depending on `process.env.NODE_ENV`, default is `development`
+
+var path = require('path')
+  , rootPath = path.normalize(__dirname + '/../..')
+
+var config = {
+  // Development config
+  //
+  development: {
+    server: {
+      port: 3001,
+      hostname: 'localhost',
+    },
+    database: {
+      url: 'mongodb://localhost/express4_bootstrap_starter'
+    },
+    BaseApiURL : 'http://localhost:3001/api/',
+    root     : rootPath,
+    app      : {
+      name : 'Express4-Bootstrap-Starter'
+    },
+    twitterAuth: true,
+    twitter: {
+      consumerKey: process.env.TWITTER_KEY || 'xxxxxxxxxxx',
+      consumerSecret: process.env.TWITTER_SECRET  || 'xxxxxxxxxxx',
+      callbackURL: '/auth/twitter/callback',
+      passReqToCallback: true
+    },
+    facebookAuth: true,
+    facebook: {
+      clientID: process.env.FACEBOOK_ID || 'xxxxxxxxxxx',
+      clientSecret: process.env.FACEBOOK_SECRET || 'xxxxxxxxxxx',
+      callbackURL: '/auth/facebook/callback',
+      passReqToCallback: true
+    }
+  },
+  //
+  // Production Config
+  //
+  production: {
+    server: {
+      port: 3001,
+      hostname: process.env.HOSTNAME || '127.0.0.1',
+    },
+    database: {
+      url: 'mongodb://localhost/express4_bootstrap_starter'
+    },
+    BaseApiURL : 'http://localhost:3001/api/',
+    root     : rootPath,
+    app      : {
+      name : 'Express4-Bootstrap-Starter'
+    },
+    twitterAuth: true,
+    twitter: {
+      // https://apps.twitter.com/app/6070534/keys
+      consumerKey: process.env.TWITTER_KEY || 'xxxxxxxxxxx',
+      consumerSecret: process.env.TWITTER_SECRET  || 'xxxxxxxxxxx',
+      callbackURL: '/auth/twitter/callback',
+      passReqToCallback: true
+    },
+    facebookAuth: true,
+    facebook: {
+      clientID: process.env.FACEBOOK_ID || 'xxxxxxxxxxx',
+      clientSecret: process.env.FACEBOOK_SECRET || 'xxxxxxxxxxx',
+      callbackURL: '/auth/facebook/callback',
+      passReqToCallback: true
+    }
+  },
+
+  //
+  // Testing config
+  //
+  test: {
+    server: {
+      port: 4001,
+      hostname: 'localhost',
+    },
+    database: {
+      url: 'mongodb://localhost/express_test'
+    }
+  }
+};
+
+module.exports = config[process.env.NODE_ENV || 'development'];

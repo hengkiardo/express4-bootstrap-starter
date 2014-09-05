eb693f1741b10bbd8ce66155092d56689f15a029
Hengkiardo <hengki.sihombing@gmail.com>
2014-09-06 01:03:03 +0700

    missing add utils helper

---
+++ app/helper/utils.js
@@ -0,0 +1,24 @@
+
+exports.randomString = function (length) {
+  var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHUJKLMNOPQRSTUVWXYZ';
+  var result = '';
+  for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+  return result;
+}
+
+exports.prettyJSON = function(data) {
+  return require("prettyjson").render(data);
+}
+
+exports.responses = function(res, status, obj) {
+  var resultPrint     = {}
+  if (status == 200) {
+    resultPrint.data = obj
+  } else {
+    resultPrint     = obj
+  }
+  resultPrint.id      = require('node-uuid').v4()
+  resultPrint.status  = status
+
+  return res.status(status).json(resultPrint )
+}

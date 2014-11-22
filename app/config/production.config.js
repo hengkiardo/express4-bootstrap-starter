9a3b20288c62a3f4f0875e6e5974f09c6935708b
Hengkiardo <hengki.sihombing@gmail.com>
2014-11-22 19:02:57 +0700

    update config for production

--- app/config/production.config.js
+++ app/config/production.config.js
@@ -3,11 +3,11 @@
 module.exports = function (ROOT_PATH) {
   var config = {
     server: {
-      port: 3001,
+      port: process.env.PORT || 3001,
       hostname: process.env.HOSTNAME || '127.0.0.1',
     },
     database: {
-      url: 'mongodb://localhost/express4_bootstrap_starter'
+      url: MONGOHQ_URL || 'mongodb://localhost/express4_bootstrap_starter'
     },
     BaseApiURL : 'http://'+process.env.HOSTNAME+':3001/api/',
     root     : ROOT_PATH,
@@ -16,22 +16,21 @@ module.exports = function (ROOT_PATH) {
     },
     twitterAuth: true,
     twitter: {
-      // https://apps.twitter.com/app/6070534/keys
-      consumerKey: process.env.TWITTER_KEY || 'zNDE0qEnZt6FdXQv60roe2p72',
-      consumerSecret: process.env.TWITTER_SECRET  || 'T6T3DzAkvz3282BCvjPRJ96WYvLPXcBY7J6MrnBng9fQq57joq',
+      consumerKey: process.env.TWITTER_KEY
+      consumerSecret: process.env.TWITTER_SECRET,
       callbackURL: '/auth/twitter/callback',
       passReqToCallback: true
     },
     facebookAuth: true,
     facebook: {
-      clientID: process.env.FACEBOOK_ID || '1405367119739358',
-      clientSecret: process.env.FACEBOOK_SECRET || '844371cba2b8192162db08b80f2b9702',
+      clientID: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
       callbackURL: '/auth/facebook/callback',
       passReqToCallback: true
     },
     mailgun: {
-      user: process.env.MAILGUN_USER || 'postmaster@tukangslicing.net',
-      password: process.env.MAILGUN_PASSWORD || '7f4v46je15w1'
+      user: process.env.MAILGUN_USER,
+      password: process.env.MAILGUN_PASSWORD
     },
     phamtom : {
       retries: 3,
